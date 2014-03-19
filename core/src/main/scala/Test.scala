efb7f1ec6987af5032f29acfb95b2611cceebdc4
Samy Dindane <samy@dindane.com>
2014-03-19 20:21:15 +0100

    -

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -1,5 +1,11 @@
 @hello
 object Test extends App {
-  case class Foo(bar: String = "hello")
+  case class C1()
+  case class C2(foo: C1 = C1(), bar: Int, baz: String = "hello")
   println(hello)
 }
+//
+//case class C(x: Int = 2, y: String, z: Boolean = true)(t: String = "hello")
+//object Test1 extends App {
+//  println(Macros.extractor[C])
+//}
--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -5,15 +5,24 @@ import scala.annotation.StaticAnnotation
 object helloMacro {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
-    import Flag._
+//    import Flag._
+    def defaultValFromValDef(valDef: ValDef): Option[String] = {
+      val value = valDef.toString().split(" = ")(1)
+      if (value == "_") None else Some(value)
+    }
     val result = {
       annottees.map(_.tree).toList match {
         case q"object $name extends ..$parents { ..$body }" :: Nil =>
           val newBody = body.map {
-            case q"case class $name(..$args)" =>
+            case q"case class $name(..$args)" => {
+              args.foreach { arg: ValDef =>
+//                println(defaultValFromValDef(arg))
+              }
+              println("==== " + args)
               q"""
               case class $name(..$args)
               """
+            }
             case x => x
           }
           q"""
@@ -26,6 +35,30 @@ object helloMacro {
     }
     c.Expr[Any](result)
   }
+}
+
+object Macros {
+  def impl[T](c: Context)(T: c.WeakTypeTag[T]): c.Expr[Map[String, Any]] = {
+    import c.universe._
+    val classSym = T.tpe.typeSymbol
+    val moduleSym = classSym.companionSymbol
+    val apply = moduleSym.typeSignature.declaration(newTermName("apply")).asMethod
+    // can handle only default parameters from the first parameter list
+    // because subsequent parameter lists might depend on previous parameters
+    println("====================================")
+    val kvps = apply.paramss.head.map(_.asTerm).zipWithIndex.flatMap{ case (p, i) =>
+      println(p.getClass)
+      if (!p.isParamWithDefault) None
+      else {
+        val getterName = newTermName("apply$default$" + (i + 1))
+        println("= " + getterName)
+        Some(q"${p.name.toString} -> $moduleSym.$getterName")
+      }
+    }
+    c.Expr[Map[String, Any]](q"Map[String, Any](..$kvps)")
+  }
+
+  def extractor[T]: Map[String, Any] = macro impl[T]
 }
 
 class hello extends StaticAnnotation {
